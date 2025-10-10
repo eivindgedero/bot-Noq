ad089d8ddf167eec415841e7921a6fe8101ae37f
Eivind Gederø <eivind.torgersen@hotmail.com>
2025-10-10 12:02:43 +0200

    Deleting outdated code and minor improvements to functions

--- bot-Noq.py
+++ bot-Noq.py
@@ -5,6 +5,8 @@ import config
 
 client = discord.Client(intents=discord.Intents.all())
 
+image_links = ["puu", "imgur", "gyazo", "streamable"]
+
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
@@ -57,7 +59,7 @@ async def on_message(message):
         return
     msg = message.content
 
-    if ((message.channel.id == 744692129812447343) and message.attachments) or ((message.channel.id == 744692129812447343) and any(link in msg for link in links_to_copy)):
+    if message.channel.id == 744692129812447343 and (message.attachments or any(link in msg for link in image_links)):
         await message.add_reaction("<a:stitch_cheer:806648037908807739>")
         await message.add_reaction("<a:panda_w00t:842286461990600714>")
 
@@ -67,90 +69,14 @@ async def on_message(message):
         if number == 4:
             await message.channel.send(file=discord.File("images/abba_dutch.png"))
 
-    if message.content.startswith("$abbalate"):
-      await message.channel.send("<@185793621524611081> wake up, it's time to game!")
-
-    # Abba - inside jokes that are still interracted with
     if message.author.id == 185793621524611081:
         number = random.randint(1, 10)
-        # if number == 2:
-        #   await message.channel.send("<:abba_approved:753774504873820251> <:abba_soup:753775919587393546>")
         wah = re.findall('[Ww][Aa]+[Hh]+', msg)
         if any(waah in msg for waah in wah):
             await message.channel.send(wah[-1] + " " + wah[-1])
-            await message.add_reaction("<:abba_wah:892937828500144169>")
         emotes = re.findall(r'[Ww][Aa][Tt].*', msg)
         for e in emotes:
             if number == 3:
                 await message.channel.send("<:what:966402492504092692>")
-   
-   
-    #Temporary for fun reactions, not really used anymore
-    # Andria
-    # if message.author.id == 207554978133442561:
-    #   number = random.randint(1,30)
-    #   if number == 5:
-    #     await message.channel.send("<:Orlando_Panda:947349761021837363> <:andria_spoon:949735041049718804>")
-    #   emotes = re.findall(r':.*cat.*:', msg)
-    #   for e in emotes:
-    #     await message.channel.send("<:coco_zbfa:844717311574802433>")
-    #   number = random.randint(1,100)
-    #   if number == 69:
-    #     await message.add_reaction("<:meow_lub:809896200290959431>")
-
-    # Melvin
-    # if message.author.id == 275134985869197312:
-    #   number = random.randint(1,9)
-    #   if number == 3:
-    #     await message.channel.send("<:mulvin:879195318057578516>")
-
-    # Danny
-    # if message.author.id == 234623956869447680:
-    #   number = random.randint(1,40)
-    #   if number == 2:
-    #     await message.add_reaction("<:danny:803790234265452575>")
-    #   elif number == 4:
-    #     await message.channel.send("<a:pepe_nerd:913165676070445157>")
-    #   elif number == 5:
-    #     await message.add_reaction("<a:danny_mald:957097485782880276>")
-
-    # Soul
-    # if message.author.id == 223280159967543296:
-    #   emotes = re.findall(r'.*[Ss][Uu][Ss].*', msg)
-    #   for e in emotes:
-    #     await message.send("<:snakie2:830485528821760040>")
-    #   number = random.randint(1,30)
-    #   if number == 4:
-    #     await message.send("<:snakie:812761513868918844>")
-    #   elif number == 6:
-    #     await message.send("<:snakie3:830485522702008351>")
-
-    # Hex
-    # if message.author.id == 159623799459938304:
-    #   emotes = re.findall(r':[Ss]orn.+:', msg)
-    #   for e in emotes:
-    #     await message.channel.send("<:ThaisOut:951628888457482340>")
-
-    # Ryan
-    # if message.author.id == 131586629541429248:
-    #   if bool(re.search(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+' , msg)) == True:
-    #     number = random.randint(1,4)
-    #     if number == 2:
-    #       await message.channel.send("<:ryan:803786019630481408>")
-
-    # Dan
-    # if message.author.id == 168793407249055744:
-    #   if bool(re.search("amazing day", msg)) == True:
-    #     await message.reply("You too! <:cat_heart:753781525342060545>")
-
-    # if bool(re.search(r'.olak', msg)) == True:
-    #   number = random.randint(1,5)
-    #   if number == 4:
-    #     await message.reply("https://puu.sh/IFPuo/d644ffe5bb.png%7C%7C")
-
-    # Eivind - troubleshooting and development placeholder
-    # if message.author.id == 157211170233647104:
-        # await message.reply("<:snakie2:830485528821760040>")
-        # await message.reply("https://puu.sh/IFPuo/d644ffe5bb.png%7C%7C")
 
 client.run(config.secret)
